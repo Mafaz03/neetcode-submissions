565fb076d26d3abe85ef61cc248be415403808fb
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2026-05-31 10:49:50 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # if len(position) == 1: return 1
+        sorted_arr = sorted(zip(position, speed), reverse=True)
+        
+        pos = []
+        spe = []
+
+        for i in sorted_arr:
+            pos.append(i[0])
+            spe.append(i[1])
+
+        unit_times = []
+        for idx in range(len(pos)):
+            t = (target - pos[idx])/spe[idx]
+            unit_times.append(t)
+        
+
+        stack = []
+        stack.append(unit_times[0])
+
+        
+        for idx in range(0, len(pos)):
+            if stack and unit_times[idx] > stack[-1]:
+                stack.append(unit_times[idx])
+
+        print(stack)
+        return len(stack)
+
+
+
+
+

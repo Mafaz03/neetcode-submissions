c62643666df8a0b4cdb91b7bda45982608232838
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2026-07-31 12:27:41 +0530

    Add: matchsticks-to-square - submission-10

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-10.py
@@ -0,0 +1,31 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        seen = set()
+        matchsticks.sort(reverse=True)
+        target = sum(matchsticks)/4
+
+        if not target.is_integer(): return False
+        else: target = int(target)
+
+        def backtrack(sides, idx):
+    
+            if all(side == target for side in sides):
+                return True
+
+            if idx == len(matchsticks):
+                return False
+
+            if tuple(sides) not in seen:
+                for s in range(4):
+                    if sides[s] + matchsticks[idx] <= target:
+                        sides[s] += matchsticks[idx]
+
+                        if backtrack(sides, idx+1): 
+                            return True
+
+                        sides[s] -= matchsticks[idx]
+            seen.add(tuple(sides))
+            
+            return False
+
+        return backtrack([0,0,0,0], 0)

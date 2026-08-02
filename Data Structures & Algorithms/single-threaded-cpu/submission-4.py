fefe720ff5805f48fd6bb53548def13bb985f53a
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2026-08-02 09:20:28 +0530

    Add: single-threaded-cpu - submission-4

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-4.py
@@ -0,0 +1,38 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        tasks = [i + [idx] for idx, i in enumerate(tasks)]
+
+        tasks = sorted(tasks, key = lambda x: x[0])
+        
+        def flip(x): return [x[1], x[2], x[0]]
+
+        heap = []
+
+        tasks_idx = 0
+        time = tasks[0][0]
+
+        while (tasks_idx < len(tasks)) and (tasks[tasks_idx][0] <= time):
+            heapq.heappush(heap, flip(tasks[tasks_idx]))
+            tasks_idx += 1
+
+        idx = []
+
+
+        while heap:
+            p_time, index, enq_time = heapq.heappop(heap)
+            idx.append(index)
+
+            time += p_time
+
+            while tasks_idx < len(tasks) and tasks[tasks_idx][0] <= time:
+                heapq.heappush(heap, flip(tasks[tasks_idx]))
+                tasks_idx += 1
+            
+            if (not heap) and (tasks_idx < len(tasks)): # heap is empty but the tasks are not done
+                time = tasks[tasks_idx][0]
+
+                while tasks_idx < len(tasks) and tasks[tasks_idx][0] <= time:
+                    heapq.heappush(heap, flip(tasks[tasks_idx]))
+                    tasks_idx += 1
+
+        return idx

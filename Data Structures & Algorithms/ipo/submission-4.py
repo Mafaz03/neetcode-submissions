bd5b568fb35598f6e482d4d6b3ea7f10f3eda6da
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2026-08-05 11:45:51 +0530

    Add: ipo - submission-4

---
+++ Data Structures & Algorithms/ipo/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+
+        heap = []
+        
+        arr = []
+        for i in range(len(profits)):
+            arr.append([capital[i], profits[i]])
+        
+        arr = sorted(arr, key = lambda x: x[0])
+
+        while (arr) and (arr[0][0] <= w):
+            heapq.heappush(heap, -arr[0][1])
+            arr = arr[1:]
+
+        
+        if heap:
+            while (k != 0):
+                w += (-heapq.heappop(heap))
+                # print(arr, heap, w)
+
+                k -= 1
+                
+                while (arr) and (arr[0][0] <= w):
+                    heapq.heappush(heap, -arr[0][1])
+                    arr = arr[1:]
+                
+        return w
+
+
+        
+
+
+
+

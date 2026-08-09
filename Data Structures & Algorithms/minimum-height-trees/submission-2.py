1591e2939ef47cfab5eb6bf34510dbdaaeab219f
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2026-08-09 14:40:39 +0530

    Add: minimum-height-trees - submission-2

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-2.py
@@ -0,0 +1,58 @@
+class Solution:
+    def __init__(self):
+        self.visited = set()
+        self.min_height = float("inf")
+
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+
+        self.adj = defaultdict(list) # {node: [connected nodes]}
+
+        for a, b in edges:
+            self.adj[a].append(b)
+            self.adj[b].append(a)
+
+        heights = defaultdict(list)
+
+        for node in self.adj:
+            self.visited.clear()
+
+            height = self.bfs(node)
+
+            if height != -1:
+
+                heights[height].append(node)
+
+                self.min_height = min(self.min_height, height)
+        
+        return heights[min(heights.keys())]
+            
+
+    def bfs(self, node):
+        height = -1
+
+        self.visited.add(node)
+        dq = deque([node])
+        
+        while dq:
+            q = len(dq)
+
+            for _ in range(q):
+                node = dq.popleft()
+                
+                for nxt in self.adj[node]:
+                    if nxt not in self.visited:
+                        self.visited.add(nxt)
+                        dq.append(nxt)
+
+            height += 1
+            if height > self.min_height: return -1
+            
+        return height
+
+
+
+
+
+        

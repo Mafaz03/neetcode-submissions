aa27243d9406a15a13a286d0b63ff6ebbcae7ca8
Mohamed Mafaz <mohdmafaz200303@gmail.com>
2026-08-31 09:42:34 +0530

    Add: excel-sheet-column-title - submission-1

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = ""
+
+        hash = {i: chr(65+i) for i in range(26)}
+
+        dummy = columnNumber 
+        while dummy:
+            res = (dummy - 1) % 26
+            result = hash[res] + result
+
+            dummy = (dummy - 1) // 26
+        return result
